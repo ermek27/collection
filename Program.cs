3a51ca35afd0c6d6e4dbf642a455ba3b661122ef
Ermek Djumataev <ermekd@Ermeks-MacBook-Pro.local>
2020-06-07 16:37:08 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectProj
+{
+    class Program
+    {
+        static string iceFlav()
+        {
+            List<string> flavors = new List<string>(){
+                "chocolate",
+                "mint",
+                "oreo",
+                "berry",
+                "cranberry",
+                "melon",
+                "creme",
+                "chunky"
+            };
+            Random rand = new Random();
+            return flavors[rand.Next(flavors.Count)];
+        }
+
+        static Dictionary<string, string> GetUsers(){
+            Dictionary<string, string> users = new Dictionary<string, string>();
+            List<string> ppl = new List<string>() {"Nick", "Romoe", "Kelly", "David", "Kolas"};
+            foreach(string person in ppl){
+                users[person] = iceFlav();
+            }
+            return users;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Dictionary<string, string> result = GetUsers();
+            Console.WriteLine(result); 
+            foreach(KeyValuePair<string, string> entry in result){
+                Console.WriteLine($"{entry.Key} likes {entry.Value}");
+            }
+        }
+    }
+}
